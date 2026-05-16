f3551660edb8176ab5030ae2275073d6e1cfe920
gomez1112 <54489633+gomez1112@users.noreply.github.com>
2026-05-16 15:37:55 -0400

    Add swift testing target

---
+++ CelmiTests/CelmiTests.swift
@@ -0,0 +1,18 @@
+//
+//  CelmiTests.swift
+//  CelmiTests
+//
+//  Created by Gerard Gomez on 5/16/26.
+//
+
+import Testing
+
+struct CelmiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
